114f4500c65f7a64bc365460df260d94bcafed54
Maria Abdullaeva <mashkoyami@gmail.com>
2022-09-05 22:14:36 +0300

    primes added

---
+++ primes.js
@@ -0,0 +1,24 @@
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+      if ( n % i == 0) return false;
+    }
+    return true;
+}
+
+function getPrimes(num) {
+    const primes = [];
+    let i = 2;
+    while (primes.length <= num) {    
+        if (isPrime(i)) primes.push(i);  
+        i++;  
+    }
+    return primes;
+}
+
+
+console.log(getPrimes(process.argv[2]))
+
+  
+
+
+
